e0adec10d9a4bc61e02cba34e4518ac73e3fc970
li ju <57431469+li-ju-bazhong@users.noreply.github.com>
2021-06-02 20:48:21 +0800

    modification
    Modified part of the code to enable it to run on python3, and added the generate_train_file.py file to generate text documents.

---
+++ generate_train_file.py
@@ -0,0 +1,22 @@
+import os
+dataset_root = "cvpr2019_PFA/ECSSD"#此处使用的是ECSSD数据集，也可以换成其他的
+img_list = []
+def check_num_images():
+
+    jpg_count = 0
+    gt_count = 0
+    for root, dirs, files in os.walk(dataset_root):
+        for fname in files:
+            if 'jpg' in fname:
+                jpg_count+=1
+                img_list.append(fname[:-4])
+            if 'png' in fname:
+                gt_count+=1
+    print ("num of images: {}, num of GT maps: {}".format(jpg_count, gt_count))
+
+check_num_images()
+
+with open("train_pair.txt", 'w+') as fout:
+    for img in img_list:
+        img_path = os.path.join(dataset_root, img)
+        fout.write(f'{img_path}.jpg {img_path}.png\n')
